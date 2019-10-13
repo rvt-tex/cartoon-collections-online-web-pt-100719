5092ae4ba9dc12c72adc162b7ca172cc86ed11aa
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 18:52:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,12 @@ def roll_call_dwarves(dwarves)
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
